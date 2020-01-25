2332cb813d74129753fdf17d7c7922fa38ddc90e
Sebastian Karlov <sebastian@MacBook-Pro.local>
2020-01-25 14:28:04 +0300

    chapter3

---
+++ chapter_3/words/count.go
@@ -0,0 +1,11 @@
+// Package words provides support for counting words
+package words
+
+import "strings"
+
+// CountWords counts the number of words in the speciefied
+// string and returns the cont
+func CountWords(text string) (cont int) {
+	count = len(strings.Fields(text))
+	return
+}
