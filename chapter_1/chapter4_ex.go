906f2f5965ad02fa7eda5b457f7093db652c68d1
Sebastian Karlov <sebastian@MacBook-Pro.local>
2020-01-29 16:19:37 +0300

    arrays

--- chapter_1/chapter4_ex.go
+++ chapter_1/chapter4_ex.go
@@ -61,4 +61,55 @@ array2 := [3]*string{new(string), new(string), new(string)}
 // copy the values from array2 into array1
 array1 = array2
 
-// 
+// declare a two dimensional integer array of four elements by two elements
+var array [4][2]int
+// use array literal to declare and initialize a two dimensional integer array
+array := [4][2]int{{10, 11}, {20, 21}, {30, 31}, {40, 41}}
+// declare and initialize index 1 and 3 of the outer array
+array := [4][2]int{1: {20, 21}, 3: {40, 41}}
+// declare and initialize individual elements of the outer and inner array
+array := [4][2]int{1: {0: 20}, 3: {1: 41}
+
+// declare a two dimensional integer array of two elements
+var array [2][2]int
+// set integer values to each individual element
+array[0][0] = 10
+array[0][1] = 20
+array[1][0] = 30
+array[1][1] = 40
+
+// declare two diffrent two dimensional integer arrays
+var array1 [2][2]int
+var array2 [2][2]int
+// add integer values to each individual element
+array2[0][0] = 10
+array2[0][1] = 20
+array2[1][0] = 30
+array2[1][1] = 40
+// copy the values from array2 into array1
+array1 = array2
+// copy index 1 of array1 into a new array of the same type
+var array3 [2]int = array1[1]
+// copy the integer found in index 1 of outer array and index 0
+// of the interior array into a new variable of type integer
+var value int = array1[1][0]
+
+// declare an array of 8 megabytes
+var array [1e6]int
+// pass the array to the function foo
+foo(array)
+// function foo accepts an array of one million integers
+func foo(array [1e6]int) {
+	...
+}
+
+// allocate an array of 8mb
+var array [1e6]int
+// pass the address of the array to the function foo
+foo(&array)
+// functoin foo accepts a pointer to an array of one million integers
+func foo(array *[1e6]int) {
+	...
+}
+
+//
