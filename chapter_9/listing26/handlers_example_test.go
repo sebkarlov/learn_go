93cff4cb21476eb3ce0c9d4549bbd36860d233eb
Sebastian Karlov <sebastian@MacBook-Pro.local>
2020-06-25 17:41:19 +0300

    book complete

---
+++ chapter_9/listing26/handlers_example_test.go
@@ -0,0 +1,29 @@
+// sample test to show how to write a basic example
+package handlers_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+)
+
+// ExampleSendJson provides a basic example
+func ExampleTestJson() {
+	r, _ := http.NewRequest("GET", "/sendjson", nil)
+	rw := httptest.NewRecorder()
+	http.DefaultServeMux.ServeHTTP(rw, r)
+
+	var u struct {
+		Name  string
+		Email string
+	}
+
+	if err := json.NewDecoder(w.Body).Decode(&u); err != nil {
+		log.Println("ERROR", err)
+	}
+
+	// use fmt to write to stdout to check the output
+	fmt.Println(u)
+}
